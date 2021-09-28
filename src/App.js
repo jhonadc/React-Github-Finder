c2b38a9f3fc74d404f43316c548bf088288d965c
Jhonathan Campos <jhonathanaugusto@gmail.com>
2021-09-27 21:32:49 -0300

    added env.local file with variables for Ghub API access

--- src/App.js
+++ src/App.js
@@ -13,7 +13,9 @@ class App extends Component {
     async componentDidMount() {
         this.setState({ loading: true });
 
-        const res = await axios.get('https://api.github.com/users');
+        const res = await axios.get(
+            `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+        );
 
         this.setState({ users: res.data, loading: false });
     }
@@ -23,7 +25,10 @@ class App extends Component {
             <div className='App'>
                 <Navbar title='Github Finder'></Navbar>
                 <div className='container'>
-                    <Users loading={this.state.loading} users={this.state.users} />
+                    <Users
+                        loading={this.state.loading}
+                        users={this.state.users}
+                    />
                 </div>
             </div>
         );
